1a05b33e89f339ebd88220e4b2102a02d7bc61c8
JunaidHassan19 <thejunni01@gmail.com>
2025-03-16 22:26:30 +0530

    Refactor App component and add custom rendering logic for React elements

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,13 @@
 
 
 function App() {
+  const name = 'JH'
+
   return (
-   <h1>Hello World! With vite | JH</h1>
+    <>
+      <h1>Hello React! With vite | {name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero quisquam amet quidem blanditiis enim cupiditate repellat recusandae distinctio deserunt, voluptates dolorum et dolor placeat! Sunt magnam consectetur dicta illo veritatis.</p>
+    </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = ' Hello JH'
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Visit Google with React Create Element',
+  anotherUser
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    // myApp()
+    // <App />
+    // anotherElement
+    reactElement
 )
